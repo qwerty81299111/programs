f72d83859cfb28f1dbdb70134f931a9c1de647ad
sergey <frants_s@bk.ru>
2022-10-16 15:32:54 +0300

    задача на нахождение квадрата числа

--- Task00/Program.cs
+++ Task00/Program.cs
@@ -1,2 +1,13 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// напишите программу, которая на вход
+// принимает число и выдает его квадрат (число
+// умноженное на само себя).
+// Например:
+// 4 -> 16
+// -3 -> 9
+// -7 -> 49
+
+
+Console.Write("ввести целое число");
+int num = Convert.ToInt32(Console.ReadLine());
+int square = num * num;
+Console.WriteLine(num = square);
