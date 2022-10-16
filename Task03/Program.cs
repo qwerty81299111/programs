01aca737b6b5f867b583161e8a05849ea645fb0d
sergey <frants_s@bk.ru>
2022-10-16 16:59:57 +0300

    ввод одного числа с выводом промежутков

---
+++ Task03/Program.cs
@@ -0,0 +1,23 @@
+﻿// напишите программу, которая а вход принимает одно число (N)
+// а на выходе показывает все целые числа в промежутке от -N до N/
+// 4-> "-4 -3 -2 -1 0 1 2 3 4"
+
+Console.Write("введите число");
+int a = Convert.ToInt32(Console.ReadLine());
+
+if (a < 0)
+    Console.Write("недопустимое число");
+else
+{
+    int count = -a;
+
+    while (count <= a)
+    {
+        Console.Write($"{count} ");
+        count++;
+        
+    }
+
+}
+
+
