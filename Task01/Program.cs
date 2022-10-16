5baf74b9407d1ac5d2adec6793917a673c51beaf
sergey <frants_s@bk.ru>
2022-10-16 15:57:14 +0300

    проверка числа на нахождение квадрата

---
+++ Task01/Program.cs
@@ -0,0 +1,19 @@
+﻿// напишите программу, которая на вход принимает два числа и проверяет, является ли первое число квадратом второгго
+// a = 25, b = 5 -> da
+// a = 2, b = 10 -> net
+// a = 9, b = -3 -> da
+// a = -3, b = 9 -> net
+
+Console.Write("введите число №1:  ");
+int a = Convert.ToInt32(Console.ReadLine());
+Console.Write("введите число №2:  ");
+int b = Convert.ToInt32(Console.ReadLine());
+
+if (a==b*b)
+{ 
+    Console.Write("да");
+}
+else
+{
+    Console.Write("нет");
+}
