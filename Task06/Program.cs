ea6fb05c0c0427fb044c578a92834320f2fb2385
sergey <frants_s@bk.ru>
2022-10-17 22:04:15 +0300

    выводит трезначное число и удаляет вторую цифру

---
+++ Task06/Program.cs
@@ -0,0 +1,16 @@
+﻿// программа, которая выводит случайное трехзначное число и удаляет 2 цифру
+// 456 -> 46
+
+
+
+int FirstLastNumber(int number)
+{
+    int firstNumber = number / 100;
+    int lastNumber = number % 10;
+    return firstNumber*10 + lastNumber;
+}
+
+int num = new Random().Next(100,1000);
+Console.WriteLine(num);
+
+Console.WriteLine(FirstLastNumber(num));
