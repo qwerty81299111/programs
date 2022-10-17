7bafa48d86c2d6dc26008c9801bbd7c931903639
sergey <frants_s@bk.ru>
2022-10-17 22:26:25 +0300

    программа, которая принимает на вход число и проверяет, кратно ли оно одновременно 7 и 23

---
+++ Task07/Program.cs
@@ -0,0 +1,14 @@
+﻿// программа, которая принимает на вход число и проверяет, кратно ли оно одновременно 7 и 23
+// 161 
+// 46
+
+Console.Write("введите число");
+int num = Convert.ToInt32(Console.ReadLine());
+
+bool Multiple(int number)
+{
+    return number % 7 == 0 && number % 23 == 0;
+}
+
+bool multiple = Multiple(num);
+Console.WriteLine(multiple? "да" : "нет");
