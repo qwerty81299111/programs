21db5c2051230d7a3a26c01f994b84c1e55def6e
sergey <frants_s@bk.ru>
2022-10-17 20:53:21 +0300

    выводит случайное число из отрезка и показывает наибольшую цифру

---
+++ Task05/Program.cs
@@ -0,0 +1,12 @@
+﻿// программа, которая выводит случайное число из отрезка 10,99 и показать наибольшую цифру
+
+// 78->8
+
+int num = new Random().Next(10,100);
+Console.WriteLine($"случайное число из диапазона 10 -99 ->{num}");
+
+int firstDigit = num/10;
+int secondDigit = num % 10; //78%10=8
+
+if(firstDigit > secondDigit) Console.WriteLine($"наибольшая цифра числа = {firstDigit}");
+else Console.WriteLine($"наибольшая цифра числа {num} = {secondDigit}");
