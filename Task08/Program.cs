e8bc021540e4bd1745db1b18c7049f0bc7a84b5b
sergey <frants_s@bk.ru>
2022-11-06 22:07:03 +0300

    принимает 2 числа и сообщает является ли 1 квадратом 2

---
+++ Task08/Program.cs
@@ -0,0 +1,17 @@
+﻿// программа, которая принимает на вход 2 числа и проверяет является ли одно число квадратом другого
+// 5,25 да
+// 8,9 нет
+
+
+bool Square(int number1, int number2)
+{
+    return number1*number1  == number2 || number2*number2 == number1;
+}
+
+Console.Write("введите первое число");
+int num1 = Convert.ToInt32(Console.ReadLine());
+Console.Write("введите второе число");
+int num2 = Convert.ToInt32(Console.ReadLine());
+
+bool square = Square(num1, num2);
+Console.WriteLine(square? "да":"нет");
